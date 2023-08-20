f85c3dc139623d49f4fe9802e337907f8f24ff95
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-20 01:23:21 +0000

    refactor function ♻️

--- src/reverseString.ts
+++ src/reverseString.ts
@@ -1,9 +1,5 @@
 const reverseString = (str:string):string => {
-  let reverse = ''
-  for(let i = str.length - 1; i >= 0;i--){
-    reverse += str[i]
-  }
-  return reverse
+  return str.split('').reverse().join('')
 }
 
 module.exports = reverseString
