f7f0fadca0602424dd99932f63be3224e3cef574
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-08-29 03:57:36 +0000

    refactor code ♻️

--- src/titleCase.ts
+++ src/titleCase.ts
@@ -1,3 +1,3 @@
-const titleCase = (str) => str.toLowerCase().split(' ').map(e => e[0].toUpperCase() + e.slice(1)).join(' ') 
+const titleCase = (str:string):string => str.toLowerCase().split(' ').map(e => e[0].toUpperCase() + e.slice(1)).join(' ') 
 
 module.exports = titleCase;
