8fff0a4b8b0e5d1c61c9486644e51f9570f83d79
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-05 11:06:10 +0000

    add diffArray func and test ✅

--- tests/diffArray.test.ts
+++ tests/diffArray.test.ts
@@ -2,6 +2,15 @@ const diffArray = require('../src/diffArray')
 
 test('return items only found in one array', ()=> {
   // arrange
-  const arr1Value=[1, 2, 3, 5]
-  const arr2Value=[1, 2, 3, 4, 5])
+  const arr1Values = [[1, 2, 3, 5],[1, "calf", 3, "piglet"]]
+  const arr2Values = [[1, 2, 3, 4, 5],[1, "calf", 3, 4]]
+  const expectedValues = [[4],["piglet",4]]
+
+  expectedValues.forEach((expectedValue,index) => {
+    // act
+    const actualValue = diffArray(arr1Values[index],arr2Values[index])
+
+    // assert
+    expect(actualValue).toEqual(expectedValue)
+  })
 })
