0e3a25fee2594cf697d60757d9926815820c908f
Derajatul <101638355+Derajatul@users.noreply.github.com>
2023-09-05 08:50:24 +0000

    add diffArray test ✅

---
+++ tests/diffArray.test.ts
@@ -0,0 +1,7 @@
+const diffArray = require('../src/diffArray')
+
+test('return items only found in one array', ()=> {
+  // arrange
+  const arr1Value=[1, 2, 3, 5]
+  const arr2Value=[1, 2, 3, 4, 5])
+})
