4c59c340aec0bfdbe69abb013a9342a4e41a8122
Moh Derajatul Ula <derajatul@gmail.com>
2024-01-13 23:31:17 +0700

    solve twoSum

---
+++ src/leetcode/twoSum.ts
@@ -0,0 +1,11 @@
+function twoSum(nums: number[], target: number): number[] {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target && i !== j) {
+        return [i, j];
+      }
+    }
+  }
+}
+const result = twoSum([3, 2, 4], 6);
+result;
