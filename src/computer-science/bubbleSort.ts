8266ab3dc955a1374d4fe4dc565c037053e05ff9
Moh Derajatul Ula <derajatul@gmail.com>
2024-03-10 23:24:31 +0700

    Add bubble sort algorithm implementation

---
+++ src/computer-science/bubbleSort.ts
@@ -0,0 +1,16 @@
+function bubbleSort(nums) {
+  let swapped = false;
+  do {
+    swapped = false;
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i + 1]) {
+        let temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
+  return nums;
+}
+console.log(bubbleSort([10, 2, 3, 5, 6, 1, 4, 7, 8, 9]));
