ebd429818b57e8be0ba8fe8d7f2f948894786371
Moh Derajatul Ula <derajatul@gmail.com>
2024-03-21 21:48:24 +0700

    solve 1 easy leetcode

---
+++ src/leetcode/climbStairs.ts
@@ -0,0 +1,10 @@
+function climbStairs(n: number): number {
+  const dp = new Array(n + 1).fill(0);
+  dp[0] = 1;
+  dp[1] = 1;
+  for (let i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
+}
+console.log(climbStairs(2)); //2
