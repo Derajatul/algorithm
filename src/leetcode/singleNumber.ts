d566901bf0859ab4a03477a8db380594255bc0e2
Moh Derajatul Ula <derajatul@gmail.com>
2024-03-26 23:49:29 +0700

    solve 1 leetcode

--- src/leetcode/singleNumber.ts
+++ src/leetcode/singleNumber.ts
@@ -1,17 +1,8 @@
 function singleNumber(nums: number[]): number {
-  let single: number[] = [];
-
-  nums.forEach((v: number) => {
-    const index = single.indexOf(v);
-
-    if (index !== -1) {
-      // Jika v sudah ada dalam array single, hapus v dari array.
-      single = single.slice(0, index).concat(single.slice(index + 1));
-    } else {
-      // Jika v belum ada dalam array single, tambahkan v.
-      single.push(v);
-    }
-  });
-  return single[0];
+  let result = 0;
+  for (let i = 0; i < nums.length; i++) {
+    result ^= nums[i];
+  }
+  return result;
 }
 console.log(singleNumber([4, 1, 2, 1, 2]));
