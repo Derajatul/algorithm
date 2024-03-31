f5f49b2560192a70daa21430db2216b8fa74ee86
Moh Derajatul Ula <derajatul@gmail.com>
2024-03-31 23:36:37 +0700

    solve 1 intermediate freecodecamp

--- src/freecodecamp/myReplace.ts
+++ src/freecodecamp/myReplace.ts
@@ -1,20 +1,11 @@
 function myReplace(str: string, before: string, after: string) {
-  function myReplace(str, before, after) {
-    // Find index where before is on string
-    var index = str.indexOf(before);
-    // Check to see if the first letter is uppercase or not
-    if (str[index] === str[index].toUpperCase()) {
-      // Change the after word to be capitalized before we use it.
-      after = after.charAt(0).toUpperCase() + after.slice(1);
-    } else {
-      // Change the after word to be uncapitalized before we use it.
-      after = after.charAt(0).toLowerCase() + after.slice(1);
-    }
-    // Now replace the original str with the edited one.
-    str = str.replace(before, after);
-
-    return str;
+  const index = str.indexOf(before);
+  if (str[index] === str[index].toUpperCase()) {
+    after = after.charAt(0).toUpperCase() + after.slice(1);
+  } else {
+    after = after.charAt(0).toLowerCase() + after.slice(1);
   }
+  return str.replace(before, after);
 }
 
 console.log(
---
+++ src/freecodecamp/pigLatin.ts
@@ -0,0 +1,20 @@
+function translatePigLatin(str) {
+  var pigLatin = "";
+  var regex = /[aeiou]/gi;
+
+  if (str[0].match(regex)) {
+    pigLatin = str + "way";
+  } else if (str.match(regex) === null) {
+    pigLatin = str + "ay";
+  } else {
+    var vowelIndice = str.indexOf(str.match(regex)[0]);
+
+    pigLatin = str.substr(vowelIndice) + str.substr(0, vowelIndice) + "ay";
+  }
+
+  return pigLatin;
+}
+
+// test here
+translatePigLatin("consonant");
+console.log(translatePigLatin("california"));
