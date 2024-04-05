9884162545642ffc97398db37230f0fccad8dd33
Moh Derajatul Ula <derajatul@gmail.com>
2024-04-05 19:53:12 +0700

    solve 1 easy leetcode

---
+++ src/leetcode/mergeSortedArray.ts
@@ -0,0 +1,26 @@
+/**
+ Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  let i = m - 1;
+  let j = n - 1;
+  let k = m + n - 1;
+
+  while (i >= 0 && j >= 0) {
+    if (nums1[i] > nums2[j]) {
+      nums1[k] = nums1[i];
+      i--;
+    } else {
+      nums1[k] = nums2[j];
+      j--;
+    }
+    k--;
+  }
+
+  while (j >= 0) {
+    nums1[k] = nums2[j];
+    j--;
+    k--;
+  }
+}
+console.log(merge([1, 2, 3, 0, 0, 0], 3, [2, 5, 6], 3))
